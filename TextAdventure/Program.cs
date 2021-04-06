a43e3d18f12f663d7b031b90180a1f9f73749827
Kemuro <antonkemuro@gmail.com>
2021-04-05 21:12:05 -0500

    created Text Adventure

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hero League: The Begtinnning. \n" +
+                "This is an interactive text adventure where your choices affect the outcome of your story. \n" +
+                "");
+            Console.WriteLine("Are you ready to begin? \n" +
+                "Press enter.");
+            Console.Clear();
+            Console.WriteLine("I woke up earlier than ususal. I guess that I was too anxious to sleep, for the day has finally come. \n" +
+                "Today I will be trying out for the Hero Leauge, our world renowned superhero team. \n" +
+                "Once five year, they host a tournament to discover new potential recruits. \n" +
+                "I've been dreaming of this day even before I discovered my abilities. \n" +
+                "I was so young when they started to manifest, but have only been seriously putting \n" +
+                " them to the test for the past __________. \n" +
+                "A - few months \n" +
+                "B - year \n" +
+                "");
+
+
+
+        }
+        public void CharacterSheet()
+        {
+
+        }
+    }
+}
